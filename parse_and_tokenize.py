098722de993ac0767b6a393e230acfae98d54f16
Vidit Gupta <viditg99@gmail.com>
2015-02-13 12:23:33 +0530

    Creating atomic articles test+train and removing junk characters

---
+++ parse_and_tokenize.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from nltk import data, word_tokenize
+import random, re
+
+tokenizer = data.load('tokenizers/punkt/english.pickle')
+
+dataset = open('dataset/nysk.xml', 'r').read()
+soup = BeautifulSoup(dataset)
+
+totalFiles = int(open('parameters.txt','r').readlines()[0].strip())
+testCount = min(100, totalFiles*1/100)
+fileNumber = 0
+test = set(random.sample(list(xrange(totalFiles)), testCount))
+
+for node in soup.findAll('text'):
+	fileNumber += 1
+	if fileNumber in test:
+		textdata = open('dataset/test/'+str(fileNumber)+'.txt', 'w')
+	else:
+		textdata = open('dataset/train/'+str(fileNumber)+'.txt', 'w')
+	text =  node.text.encode('ascii', 'ignore')
+	text = text.decode('utf-8')
+	text = text.replace('\n', '')
+	sentences = tokenizer.tokenize(text)
+	for sentence in sentences:
+		sentence = sentence.replace('.', '')
+		sentence = re.sub("&apos;","'",sentence)
+		sentence = re.sub("[^\w\s']|_","",sentence)
+		sentence = sentence.lower()
+		textdata.write(sentence+'\n')
+	textdata.close()
