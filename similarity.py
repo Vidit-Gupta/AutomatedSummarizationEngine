291268219bfd850e901893aeda105b0121bc9fe6
Prateek Sachdev <prateek.sachdev93@gmail.com>
2015-02-23 19:26:39 +0530

    similarity class

--- similarity.py
+++ similarity.py
@@ -1,31 +1,55 @@
-idf = {} #should map string of word to its idf
+import gensim
 
-def get_word_simlarity(word1,word2): #should return word2vec similarity of two words
-	return 1
+class Similarity:
 
-def get_sentence_similarity(sentence1,sentence2): #sentences as list of words
-	
-	sim_1 = 0
-	for i in range(len(sentence1)):
-		max_sim = -10000000
-		for j in range(len(sentence2)):
-			val = get_word_simlarity(sentence1[i],sentence2[j])
-			if(val > max_sim):
-				max_sim = val
-		sim_1+= idf[sentence1[i]]*max_sim
-	sim_1 = sim_1*1.0/len(sentence1)	
-	
-	sim_2 = 0
-	for i in range(len(sentence2)):
-		max_sim = -10000000
-		for j in range(len(sentence1)):
-			val = get_word_simlarity(sentence1[j],sentence2[i])
-			if(val > max_sim):
-				max_sim = val
-		sim_2+= idf[sentence2[i]] * max_sim
-	sim_2 = sim_2*1.0/len(sentence2)
+	def __init__(self):
+		self.model = gensim.models.Word2Vec.load('./wordSimilarityModel')
+		self.idf_vals = open('idf.txt','r').read().split(' ')
+		self.words_vals = open('vocab.txt','r').readlines()
+		self.idf = dict(zip(self.words_vals, self.idf_vals))
 
-	return ((sim_1 + sim_2)*1.0)/2
+	def get_word_simlarity(self,word1,word2): #should return word2vec similarity of two words
+		return self.model.similarity(word1, word2)
+
+	def get_sentence_similarity(self,sentence1,sentence2): #sentences as list of words
+		
+		sim_1 = 0
+		valid_words = len(sentence1)
+		for i in range(len(sentence1)):
+			
+			if(sentence1[i] not in self.idf):
+				valid_words-=1
+				continue
+				
+			max_sim = -10000000
+			for j in range(len(sentence2)):
+				if(sentence2[i] not in self.idf):
+					continue
+				val = self.get_word_simlarity(sentence1[i],sentence2[j])
+				if(val > max_sim):
+					max_sim = val
+			sim_1+= self.idf[sentence1[i]]*max_sim
+		sim_1 = sim_1*1.0/valid_words
+		
+		sim_2 = 0
+		valid_words = len(sentence2)
+		for i in range(len(sentence2)):
+			
+			if(sentence2[i] not in self.idf):
+				valid_words-=1
+				continue
+
+			max_sim = -10000000
+			for j in range(len(sentence1)):
+				if(sentence1[i] not in self.idf):
+					continue
+				val = self.get_word_simlarity(sentence1[j],sentence2[i])
+				if(val > max_sim):
+					max_sim = val
+			sim_2+= self.idf[sentence2[i]] * max_sim
+		sim_2 = sim_2*1.0/valid_words
+
+		return ((sim_1 + sim_2)*1.0)/2
 
 
 
--- text_rank.py
+++ text_rank.py
@@ -1,49 +1,44 @@
-num_sentences = 3
-initial_score = 1
-num_of_iterations = 1
-damping_factor = 0.85
-
-scores = [initial_score for x in range(num_sentences)]
-similarity_matrix = [[0 for y in range(num_sentences)] for x in range(num_sentences)]
-
-##############################
-similarity_matrix[0][0] = 1
-similarity_matrix[0][1] = 0.6
-similarity_matrix[0][2] = 0.2
-similarity_matrix[1][0] = 0.6
-similarity_matrix[1][1] = 1
-similarity_matrix[1][2] = 0.3
-similarity_matrix[2][0] = 0.2
-similarity_matrix[2][1] = 0.3
-similarity_matrix[2][2] = 1
-
-
-
-print similarity_matrix
-
-temp_scores = [initial_score for x in range(num_sentences)] # temp scores assigned to be 0
-
-for iter_no in range(num_of_iterations):	
-	for i in range(num_sentences):
-
-		sum_val = 0.0
-		for j in range(num_sentences):
-			edge_sum = 0
-		
-			if j!=i:
-				for k in range(num_sentences):
-					edge_sum = edge_sum + similarity_matrix[j][k]
-				sum_val = sum_val + (1.0 * scores[j] * similarity_matrix[j][i])/edge_sum
-		
-		temp_scores[i] = (1-damping_factor) + (damping_factor * sum_val)
+import similarity
+def text_rank(sentences,num_of_iterations,damping_factor):
+	num_sentences = len(sentences)
+	initial_score = 1
+	# num_of_iterations = 1
+	# damping_factor = 0.85
+	similarity_obj = similarity.Similarity()
+	scores = [initial_score for x in range(num_sentences)]
+	similarity_matrix = [[0 for y in range(num_sentences)] for x in range(num_sentences)]
+	dissimilarity_matrix = [[0 for y in range(num_sentences)] for x in range(num_sentences)]
 
 	for i in range(num_sentences):
-		scores[i] = temp_scores[i]    # setting the new score calculated with each iteration
-	print scores
+		for j in range(i,num_sentences):
+			similarity_matrix[i][j] = similarity_obj.get_sentence_similarity(sentences[i],sentences[j])
+			similarity_matrix[j][i] = similarity_matrix[i][j]
+			dissimilarity_matrix[i][j] = 1 - similarity_matrix[i][j]
+			dissimilarity_matrix[j][i] = 1 - similarity_matrix[i][j]
 
 
+	# print similarity_matrix
 
+	temp_scores = [initial_score for x in range(num_sentences)] # temp scores assigned to be 0
 
-# print scores
+	for iter_no in range(num_of_iterations):	
+		for i in range(num_sentences):
+
+			sum_val = 0.0
+			for j in range(num_sentences):
+				edge_sum = 0
+			
+				if j!=i:
+					for k in range(num_sentences):
+						edge_sum = edge_sum + similarity_matrix[j][k]
+					sum_val = sum_val + (1.0 * scores[j] * similarity_matrix[j][i])/edge_sum
+			
+			temp_scores[i] = (1-damping_factor) + (damping_factor * sum_val)
+
+		for i in range(num_sentences):
+			scores[i] = temp_scores[i]    # setting the new score calculated with each iteration
+
+	# print scores
+	return [scores] + [dissimilarity_matrix] # return a list containing scores and dissimilarity matrix
 
 
