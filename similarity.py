699a21ac5cba57a94b9b85db5f09ad340f463c8a
Vidit Gupta <viditg99@gmail.com>
2015-02-24 14:49:40 +0530

    Fixed similarity and text rank bugs

--- similarity.py
+++ similarity.py
@@ -5,15 +5,18 @@ class Similarity:
 	def __init__(self):
 		self.model = gensim.models.Word2Vec.load('./wordSimilarityModel')
 		self.idf_vals = open('idf.txt','r').read().split(' ')
+		self.idf_vals = map(lambda x: float(x), self.idf_vals)
 		self.words_vals = open('vocab.txt','r').readlines()
+		self.words_vals = map(lambda x: x.strip(), self.words_vals)
 		self.idf = dict(zip(self.words_vals, self.idf_vals))
 
 	def get_word_simlarity(self,word1,word2): #should return word2vec similarity of two words
 		return self.model.similarity(word1, word2)
 
 	def get_sentence_similarity(self,sentence1,sentence2): #sentences as list of words
-		
 		sim_1 = 0
+		sentence1 = sentence1.split(' ')
+		sentence2 = sentence2.split(' ')
 		valid_words = len(sentence1)
 		for i in range(len(sentence1)):
 			
@@ -23,13 +26,16 @@ class Similarity:
 				
 			max_sim = -10000000
 			for j in range(len(sentence2)):
-				if(sentence2[i] not in self.idf):
+				if(sentence2[j] not in self.idf):
 					continue
 				val = self.get_word_simlarity(sentence1[i],sentence2[j])
 				if(val > max_sim):
 					max_sim = val
 			sim_1+= self.idf[sentence1[i]]*max_sim
-		sim_1 = sim_1*1.0/valid_words
+		if valid_words == 0:
+			sim_1 = 0
+		else:
+			sim_1 = sim_1*1.0/valid_words
 		
 		sim_2 = 0
 		valid_words = len(sentence2)
@@ -41,18 +47,15 @@ class Similarity:
 
 			max_sim = -10000000
 			for j in range(len(sentence1)):
-				if(sentence1[i] not in self.idf):
+				if(sentence1[j] not in self.idf):
 					continue
 				val = self.get_word_simlarity(sentence1[j],sentence2[i])
 				if(val > max_sim):
 					max_sim = val
 			sim_2+= self.idf[sentence2[i]] * max_sim
-		sim_2 = sim_2*1.0/valid_words
+		if valid_words == 0:
+			sim_2 = 0
+		else:
+			sim_2 = sim_2*1.0/valid_words
 
-		return ((sim_1 + sim_2)*1.0)/2
-
-
-
-
-
-
+		return ((sim_1 + sim_2))/2.0
--- text_rank.py
+++ text_rank.py
@@ -1,9 +1,9 @@
 import similarity
-def text_rank(sentences,num_of_iterations,damping_factor):
+from math import fabs
+
+def text_rank(sentences,threshold,damping_factor):
 	num_sentences = len(sentences)
 	initial_score = 1
-	# num_of_iterations = 1
-	# damping_factor = 0.85
 	similarity_obj = similarity.Similarity()
 	scores = [initial_score for x in range(num_sentences)]
 	similarity_matrix = [[0 for y in range(num_sentences)] for x in range(num_sentences)]
@@ -13,15 +13,19 @@ def text_rank(sentences,num_of_iterations,damping_factor):
 		for j in range(i,num_sentences):
 			similarity_matrix[i][j] = similarity_obj.get_sentence_similarity(sentences[i],sentences[j])
 			similarity_matrix[j][i] = similarity_matrix[i][j]
-			dissimilarity_matrix[i][j] = 1 - similarity_matrix[i][j]
-			dissimilarity_matrix[j][i] = 1 - similarity_matrix[i][j]
+			dissimilarity_matrix[i][j] = -similarity_matrix[i][j]
+			dissimilarity_matrix[j][i] = -similarity_matrix[i][j]
 
 
-	# print similarity_matrix
+	print similarity_matrix
+	print '\n'
+	print '\n'
+	print dissimilarity_matrix
 
 	temp_scores = [initial_score for x in range(num_sentences)] # temp scores assigned to be 0
-
-	for iter_no in range(num_of_iterations):	
+	minimumChange = True
+	while minimumChange == True:
+		minimumChange = False
 		for i in range(num_sentences):
 
 			sum_val = 0.0
@@ -34,11 +38,10 @@ def text_rank(sentences,num_of_iterations,damping_factor):
 					sum_val = sum_val + (1.0 * scores[j] * similarity_matrix[j][i])/edge_sum
 			
 			temp_scores[i] = (1-damping_factor) + (damping_factor * sum_val)
-
 		for i in range(num_sentences):
+			if fabs(scores[i]-temp_scores[i]) > threshold:
+				minimumChange = True
 			scores[i] = temp_scores[i]    # setting the new score calculated with each iteration
 
 	# print scores
 	return [scores] + [dissimilarity_matrix] # return a list containing scores and dissimilarity matrix
-
-
